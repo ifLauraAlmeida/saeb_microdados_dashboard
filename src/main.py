3be0d00d91016cd568a8d4c65c97b30466bc2730
ifLauraAlmeida <oddlauraink@hotmail.com>
2026-02-03 21:46:40 -0300

    refactor: rename additional SAEB 1995 director subcategories

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ colspecs = [
     (56, 64),  # REDE
     (64, 72),  # UF
     (72, 80),  # REGIAO
-    (80, 88),  # SEXO (d_001)
+    (80, 88),  # SEXO 
     (88, 96),  # D_002 - Idade
     (96, 104),  # D_003 - Escolaridade
     (104, 112),  # D_004 - Escolaridade específica
@@ -33,10 +33,10 @@ names = [
     "uf",
     "regiao",
     "sexo",
-    "d_002",
-    "d_003",
-    "d_004",
-    "d_005",
+    "idade",
+    "escolaridade",
+    "esc_especifica",
+    "cursos",
 ]
 
 df = pd.read_fwf(
@@ -46,11 +46,90 @@ df = pd.read_fwf(
     na_values=["", " "],
 )
 
+subs_serie = {
+    3: "3º Ano",
+    4: "4º Série",
+    8: "8º Série"
+}
+df["serie"] = df["serie"].astype("Int64").map(subs_serie)
+
+subs_dep_adm = {
+    1: "Estadual",
+    2: "Municipal",
+    3: "Particular",
+    4: "Federal"
+}
+df["dep_adm"] = df["dep_adm"].astype("Int64").map(subs_dep_adm)
+
+subs_local = {
+    1: "Urbana",
+    2: "Rural"
+}
+df["local"] = df["local"].astype("Int64").map(subs_local)
+
+subs_rede = {
+    1: "Pública",
+    2: "Particular"
+}
+df["rede"] = df["rede"].astype("Int64").map(subs_rede)
+
+subs_uf = {
+    11:"Rondônia",
+    12:"Acre",
+    13:"Amazonas",
+    14:"Roraima",
+    15:"Pará",
+    16:"Amapá",
+    17:"Tocantins",
+    21:"Maranhão",
+    22:"Piauí",
+    23:"Ceará",
+    24:"Rio Grande do Norte",
+    25:"Paraíba",
+    26:"Pernambuco",
+    27:"Alagoas",
+    28:"Sergipe",
+    29:"Bahia",
+    31:"Minas Gerais",
+    32:"Espírito Santo",
+    33:"Rio de Janeiro",
+    35:"São Paulo",
+    41:"Paraná",
+    42:"Santa Catarina",
+    43:"Rio Grande do Sul",
+    50:"Mato Grosso do Sul",
+    51:"Mato Grosso",
+    52:"Goiás",
+    53:"Distrito Federal"
+}
+df["uf"] = df["uf"].astype("Int64").map(subs_uf)
+
+subs_regiao ={
+    1:"Norte",
+    2:"Nordeste",
+    3:"Sudeste",
+    4:"Sul",
+    5:"Centro-Oeste"
+}
+df["regiao"] = df["regiao"].astype("Int64").map(subs_regiao)
+
 subs_sexo = {
     1: "Feminino", 
     2: "Masculino"}
 df["sexo"] = df["sexo"].astype("Int64").map(subs_sexo)
 
-print(df.info())
+subs_escolaridade = {
+    1:"Nenhum",
+    2:"1º Grau Menor (Antigo Primário)",
+    3:"2º Grau Maior (Antigo Ginásio)",
+    4:"2º Grau-Magistério",
+    5:"2º Grau-Outros",
+    6:"Superior-Licenciatura",
+    7:"Superior-Outros",
+    8:"Pós-Graduação"
+}
+df["escolaridade"] = df["escolaridade"].astype("Int64").map(subs_escolaridade)
 
-print(df.head())
+subs_esc_especifica = {}
+print(df.info())
+print(df.head(40))
