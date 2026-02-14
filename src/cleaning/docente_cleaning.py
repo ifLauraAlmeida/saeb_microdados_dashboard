ca065370ca84509d2c9cc22829bbfa349e803156
ifLauraAlmeida <oddlauraink@hotmail.com>
2026-02-14 20:18:54 -0300

    feat(docente-cleaning): remove unused columns and columns with high null rates

--- src/cleaning/docente_cleaning.py
+++ src/cleaning/docente_cleaning.py
@@ -4,7 +4,23 @@ def drop_colunas_problematicas_docente(df: pd.DataFrame) -> pd.DataFrame:
     colunas_dropar = [
             "serie",
             "turma",
-            
+            "sexo",
+            "idade",
+            "tempo_ensino",
+            "grupos_sala",
+            "usa_jornais",
+            "selecionar_alunos_preparados",
+            "recurso_livros",
+            "recurso_dicionarios",
+            "recurso_quadro_giz",
+            "recurso_papel_oficio",
+            "recurso_repografia",
+            "recurso_fitas_video",
+            "assistiu_tv_escola",
+            "recurso_tv_escola",
+            "avaliacao_tv_escola",
+            "tipo_programa_tv_escola",
+            "tempo_programacao_tv_escola"
         ]
 
     df = df.drop(columns=colunas_dropar, errors="ignore")
